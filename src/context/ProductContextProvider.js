4f7b2d99bbdf8494b67b3b06cf1f7c0fe31b0564
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-03-04 00:10:15 +0330

    feat: added context to globally apply the root & shared products data

---
+++ src/context/ProductContextProvider.js
@@ -0,0 +1,25 @@
+import React, { createContext, useState, useEffect } from 'react';
+//API
+import { getProducts } from '../services/api';
+//Context
+const ProductContext = createContext();
+const ProductContextProvider = ({ children }) => {
+      //States
+      const [products, setProducts] = useState([]);
+
+      //Get and set Api data
+      useEffect(() => {
+            const fetchApi = async () => {
+                  setProducts(await getProducts());
+            };
+            fetchApi();
+      }, []);
+
+      return (
+            <div>
+                  <ProductContext.Provider value={products}>{children}</ProductContext.Provider>
+            </div>
+      );
+};
+
+export default ProductContextProvider;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ProductContextProvider from './context/ProductContextProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+      <ProductContextProvider>
+            <App />
+      </ProductContextProvider>,
+
+      document.getElementById('root'),
+);
