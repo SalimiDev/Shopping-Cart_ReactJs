5e63db06ef945942758666d28edf0bd767b1dccc
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-03-08 02:11:42 +0330

    style:added cart and shop cart component's styles

--- src/components/shared/Cart.js
+++ src/components/shared/Cart.js
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react';
+// Style
+import styles from "./Cart.module.css";
 //Function
 import { shorten } from '../../helpers/functions';
 //Icons
@@ -10,28 +12,24 @@ const Cart = ({ data }) => {
       const { title, image, price, quantity } = data;
       console.log(quantity);
       return (
-            <div>
-                  <div>
-                        <img src={image} alt='product' />
-                        <div>
-                              <h3>{shorten(title)}</h3>
-                              <p>{price} $</p>
-                        </div>
-                        <div>
-                              <span>{quantity}</span>
-                        </div>
-                        <div>
-                              {quantity > 1 ? (
-                                    <button onClick={() => dispatch({ type: 'DECREASE', payload: data })}>-</button>
-                              ) : (
-                                    <button onClick={() => dispatch({ type: 'REMOVE_ITEM', payload: data })}>
-                                          <img src={trashIcon} alt='trash' style={{ width: '20px' }} />
-                                    </button>
-                              )}
-                              <button onClick={() => dispatch({ type: 'INCREASE', payload: data })}>+</button>
-                        </div>
-                  </div>
+            <div className={styles.container} >
+            <img className={styles.productImage} src={image} alt='product' />
+            <div className={styles.data}>
+                <h3>{shorten(title)}</h3>
+                <p>{price} $</p>
             </div>
+            <div>
+                <span className={styles.quantity}>{quantity}</span>
+            </div>
+            <div className={styles.buttonContainer}>
+                {
+                    quantity > 1 ? 
+                    <button onClick={() => dispatch({type: "DECREASE", payload: data})} >-</button> :
+                    <button onClick={() => dispatch({type: "REMOVE_ITEM", payload: data})} ><img src={trashIcon} alt="trash" /></button>
+                }
+                <button onClick={() => dispatch({type: "INCREASE", payload: data})} >+</button>
+            </div>
+        </div>
       );
 };
 
